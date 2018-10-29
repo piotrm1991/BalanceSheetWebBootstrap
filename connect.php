fc519002011077588ba785f13dd7ff1661e7a9ca
Piotr <34282959+piotrm1991@users.noreply.github.com>
2018-10-29 21:11:37 +0100

    removed old version

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return ['host' => 'localhost', 
-		'user' => 'root',
-		'password' => '',
-		'database' => 'balance_personal'
-];
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-
-unset($_SESSION['logged_id']);
-unset($_SESSION['logged_login']);
-header('Location: index.php');
